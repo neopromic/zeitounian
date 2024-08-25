bb7e02bc3241a13485047feb2abc29464fa3dcdb
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-08-25 18:22:05 -0300

    feat: working on the ui

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import {
   BriefcaseBusiness,
   Trophy,
 } from "lucide-react";
+import Image from "next/image";
 
 const Texts = [
   "SUPERVISÓRIO SCADA - INDÚSTRIA 4.0",
@@ -23,7 +24,7 @@ export default function Home() {
     <main className="scroll-smooth">
       <section className="flex flex-col py-12 px-4 justify-center items-center h-64 max-h-64 relative">
         <ChangingText text={Texts} />
-        <p className="text-muted-foreground leading-relaxed tracking-normal text-xs">
+        <p className="text-muted-foreground leading-relaxed tracking-normal text-xs text-center">
           Fornecemos serviços manutenção elétrica com equipe especializada.
         </p>
         <Button className="absolute bottom-2" variant={"blue"}>
@@ -94,12 +95,90 @@ export default function Home() {
         </div>
       </section>
       <section className="flex flex-col items-center justify-center p-4">
-        <h2 className="font-semibold text-2xl tracking-tighter">Nossas principais áreas de atuação</h2>
-        <h3 className="tracking-wider dark:text-blue-300 text-blue-500 my-2 text-xs">Conheça alguns dos segmentos que atuamos</h3>
+        <h2 className="font-semibold text-2xl tracking-tighter">
+          Nossas principais áreas de atuação
+        </h2>
+        <h3 className="tracking-wider dark:text-blue-300 text-blue-500 my-2 text-xs">
+          Conheça alguns dos segmentos que atuamos
+        </h3>
         <p className="text-center text-xs text-muted-foreground">
           A Zeitounian Tecnologia é hoje uma empresa de engenharia que atende
           diversos segmentos do mercado com produtos e serviços especializados.
         </p>
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <div className="border rounded-md hover:rounded-md flex flex-col space-y-2 hover:bg-blue-900/90 transition-all shadow-md group max-w-[200px]">
+            <Image
+              src={"/assist_nobreak_estab.jpg"}
+              width={1000}
+              height={100}
+              alt="ASSISTÊNCIA TÉCNICA"
+              className="w-full bg-cover object-cover rounded-t-md border-b lg:max-w-[200px]"
+            />
+            <h2 className="w-full text-center font-semibold tracking-tight text-nowrap truncate group-hover:text-blue-50">
+              ASSISTÊNCIA TÉCNICA
+            </h2>
+            <p className="text-xs font-light text-center w-full p-2 group-hover:text-blue-50">
+              Especializados em manutenção de conversores de energia, nobreaks,
+              estabilizadores, carregadores e fontes.
+            </p>
+          </div>
+          <div className="border rounded-md hover:rounded-md flex flex-col space-y-2 hover:bg-blue-900/90 transition-all shadow-md group max-w-[200px]">
+            <Image
+              src={"/projeto_eletrico.jpg"}
+              width={1000}
+              height={100}
+              alt=""
+              className="w-full bg-cover object-cover rounded-t-md border-b lg:max-w-[200px]"
+            />
+            <h2 className="w-full text-center font-semibold tracking-tight text-nowrap truncate group-hover:text-blue-50">
+              PROJETO ELÉTRICO
+            </h2>
+            <p className="text-xs font-light text-center w-full p-2 group-hover:text-blue-50">
+              {" "}
+              Projeto de infraestrutura elétrica residencial, comercial e
+              industrial conforme as normas vigentes.
+            </p>
+          </div>
+          <div className="border rounded-md hover:rounded-md flex flex-col space-y-2 hover:bg-blue-900/90 transition-all shadow-md group max-w-[200px]">
+            <Image
+              transition-all
+              src={"/manut_eletrica.jpg"}
+              width={1000}
+              height={100}
+              alt=""
+              className="w-full bg-cover object-cover rounded-t-md border-b lg:max-w-[200px]"
+            />
+            <h2 className="w-full text-center font-semibold tracking-tight text-nowrap truncate group-hover:text-blue-50">
+              MANUTENÇÃO ELÉTRICA
+            </h2>
+            <p className="text-xs font-light text-center w-full p-2 group-hover:text-blue-50">
+              Serviços de manutenção elétrica com equipe especializada e
+              equipamentos de ponta.
+            </p>
+          </div>
+          <div className="border rounded-md hover:rounded-md flex flex-col space-y-2 hover:bg-blue-900/90 transition-all shadow-md group max-w-[200px]">
+            <Image
+              transition-all
+              src={"/sistemas_iot.jpg"}
+              width={1000}
+              height={100}
+              alt=""
+              className="w-full bg-cover object-cover rounded-t-md border-b lg:max-w-[200px]"
+            />
+            <h2 className="w-full text-center font-semibold tracking-tight text-nowrap truncate group-hover:text-blue-50">
+              IOT - INDÚSTRIA 4.0
+            </h2>
+            <p className="text-xs font-light text-center w-full p-2 group-hover:text-blue-50">
+              Projetos para clientes que necessitam de hardwares dedicado para
+              aplicações de controle e gerenciamento, entre no mundo do IOT com
+              a Zeitounian.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="p-4">
+        <h2 className="text-2xl font-bold tracking-tighter w-full text-center">Depoimentos</h2>
+
       </section>
     </main>
   );
