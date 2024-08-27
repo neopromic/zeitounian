2b3fc9b4a40524683cfaf24ea744d8bf5af78270
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-08-27 15:14:27 -0300

    feat: add a custom url path at route institucional page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/institucional',
+          destination: '/PageInstitucional',
+        },
+      ];
+    },
+  
+};
 
 export default nextConfig;
